6edb5f34451b02c5dd193f763e718bc106950b95
FelipeRRMiotto <felipe.miotto@germinare.org.br>
2024-10-03 09:27:34 -0300

    commitando

--- rpa_bancos.py
+++ rpa_bancos.py
@@ -1,7 +1,7 @@
-from datetime import date
+from datetime import datetime, timedelta
 import psycopg2 as pg
 
-hoje = str(date.today())
+ontem = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
 host_cloud = "pg-11d01e0e-testepgsql.e.aivencloud.com"
 porta = "24931"
 usuario = "avnadmin"
@@ -12,6 +12,7 @@ dev1 = 'banco1_dev'
 dev2 = 'banco2_dev'
 
 
+#O loop roda 2 vezes, a primeira no ambiente produtivo e a segunda no ambiente de dev
 for amb in range(0,2):
     if amb == 0:
         database1 = prod1
@@ -21,30 +22,150 @@ for amb in range(0,2):
         database2 = dev2
 
     try:
+        #criando conexões no banco
         conn1 = pg.connect(host=host_cloud, database=database1, user=usuario, password=senha, port=porta)
         conn2 = pg.connect(host=host_cloud, database=database2, user=usuario, password=senha, port=porta)
         cur1 = conn1.cursor()
         cur2 = conn2.cursor()
 
-        cur1.execute('select pk_int_id_cor_mascote, text_fundo, text_secundaria, text_primaria from tb_cor_mascote where createdAt < %s',(hoje,))
+        #============================================================================================
+        #Passando novos registros do banco do 1° para o banco do 2°
+        #============================================================================================
+
+        #Obtendo dados da tabela tb_cor_mascote do banco do 1° e passando para o banco do 2°
+        cur1.execute('select pk_int_id_cor_mascote, text_fundo, text_secundaria, text_primaria, deletedAt from tb_cor_mascote where createdAt = %s',(ontem,))
         cores = cur1.fetchall()
 
+        #Verificando se algum novo registro foi encontrado
         if len(cores) > 0:
-            query = "INSERT INTO tb_cor_araci(pk_int_id_cor_araci, var_fundo, var_secundaria, var_primaria) values "
+            #Inserindo todos os registros encontrados no banco do 2°
+            query = "INSERT INTO tb_cor_araci(pk_int_id_cor_araci, var_fundo, var_secundaria, var_primaria, deletedAt) values "
             for i in cores:
-                    query = query+"("+i[0]+","+i[1]+","+i[2]+","+i[3]+"),"
-            query = query[:-1]
+                    query = query+"("+str(i[0])+",'"+str(i[1])+"','"+str(i[2])+"','"+str(i[3])+"',"+str(i[4])+"),"
+            query = (query[:-1]).replace("None","null")
             cur2.execute(query)
+            conn2.commit()
 
-        cur1.execute('select dt_data_inicio, dt_data_final, var_nome, var_local, num_preco_ticket, pk_int_id_evento, fk_int_id_usuario from tb_evento where createdAt < %s',(hoje,))
+        #===========================================================================================
+
+        #Obtendo dados da tabela tb_evento do banco do 1° e passando para o banco do 2°
+        cur1.execute('select dt_data_inicio, dt_data_final, var_nome, var_local, num_preco_ticket, pk_int_id_evento, fk_int_id_usuario, deletedat from tb_evento where createdAt = %s',(ontem,))
+        eventos = cur1.fetchall()
+
+        #Verificando se algum novo registro foi encontrado
+        if len(eventos) > 0:
+            #Inserindo todos os registros encontrados no banco do 2°
+            query = "INSERT INTO tb_evento(dt_data_inicio, dt_data_final, var_nome, var_local, float_preco_ticket, pk_int_id_evento, fk_int_id_usuario, deletedat) values "
+            for i in eventos:
+                    query = query+"('"+str(i[0])+"','"+str(i[1])+"','"+str(i[2])+"','"+str(i[3])+"',"+str(i[4])+","+str(i[5])+","+str(i[6])+","+str(i[7])+"),"
+            query = (query[:-1]).replace("None","null")
+            cur2.execute(query)
+            conn2.commit()
+
+        #===========================================================================================
+
+        #Obtendo dados da tabela tb_barraca do banco do 1° e passando para o banco do 2°
+        cur1.execute('select pk_int_id_barraca, var_nome, fk_int_id_evento, deletedat from tb_barraca where createdAt = %s',(ontem,))
+        barracas = cur1.fetchall()
+
+        #Verificando se algum novo registro foi encontrado
+        if len(barracas) > 0:
+            #Inserindo todos os registros encontrados no banco do 2°
+            query = "INSERT INTO tb_barraca(pk_int_id_barraca, var_nome, fk_int_id_evento, deletedAt) values "
+            for i in barracas:
+                    query = query+"("+str(i[0])+",'"+str(i[1])+"',"+str(i[2])+","+str(i[3])+"),"
+            query = (query[:-1]).replace("None","null")
+            cur2.execute(query)
+            conn2.commit()
+        
+        #============================================================================================
+        #Passando registros recentemente alterados do banco do 1° para o banco do 2°
+        #============================================================================================
+
+        #Obtendo dados atualizados da tabela tb_cor_mascote do banco do 1° e passando para o banco do 2°
+        cur1.execute('select pk_int_id_cor_mascote, text_fundo, text_secundaria, text_primaria, deletedAt from tb_cor_mascote where updatedat = %s order by pk_int_id_cor_mascote',(ontem,))
         cores = cur1.fetchall()
 
+        #Verificando se algum novo registro foi encontrado
         if len(cores) > 0:
-            query = "INSERT INTO tb_cor_araci(pk_int_id_cor_araci, var_fundo, var_secundaria, var_primaria) values "
+            lista_cols = ["var_primaria","var_secundaria","var_fundo","deletedAt"]
+            lista_mods = []
+            querySelect = "select var_fundo, var_secundaria, var_primaria, deletedAt from tb_cor_araci where pk_int_id_cor_araci in ("
             for i in cores:
-                    query = query+"("+i[0]+","+i[1]+","+i[2]+","+i[3]+"),"
-            query = query[:-1]
-            cur2.execute(query)
+                querySelect = querySelect+i[0]+","
+            querySelect = (querySelect[:-1])+") order by pk_int_id_cor_araci"
+            cur2.execute(querySelect)
+            cores2 = cur2.fetchall()
+
+            for i in cores2:
+                for c in range(0,len(i)):
+                    if i[c] != cores[c+1]:
+                        lista_mods.append([lista_cols[c],cores[c+1],i[0]])
+            
+            for i in lista_mods:
+                query = ("update tb_cor_araci set "+str(i[0])+" = "+str(i[1])+" where pk_int_id_cor_araci = "+str(i[2])).replace("None","null")
+                cur2.execute(query)
+            conn2.commit()
+
+        #===========================================================================================
+
+        #Obtendo dados atualizados da tabela tb_evento do banco do 1° e passando para o banco do 2°
+        cur1.execute('select pk_int_id_evento, dt_inicio, dt_final, var_nome, var_local, num_preco_ticket, deletedat, fk_int_id_usuario from tb_evento where updatedat = %s order by pk_int_id_evento',(ontem,))
+        eventos = cur1.fetchall()
+
+        #Verificando se algum novo registro foi encontrado
+        if len(eventos) > 0:
+            lista_cols = ["dt_inicio", "dt_final", "var_nome", "var_local", "float_preco_ticket", "deletedat", "fk_int_id_usuario"]
+            lista_mods = []
+            querySelect = "select dt_inicio, dt_final, var_nome, var_local, num_preco_ticket, deletedat, fk_int_id_usuario from tb_evento where pk_int_id_evento in ("
+            for i in eventos:
+                querySelect = querySelect+i[0]+","
+            querySelect = (querySelect[:-1])+") order by pk_int_id_evento"
+            cur2.execute(querySelect)
+            eventos2 = cur2.fetchall()
+
+            for i in eventos2:
+                for c in range(0,len(i)):
+                    if i[c] != eventos[c+1]:
+                        if c in [4,6]:
+                            lista_mods.append([lista_cols[c],eventos[c+1],i[0]])
+                        else:
+                            lista_mods.append([lista_cols[c],"'"+str(eventos[c+1])+"'",i[0]])
+            
+            for i in lista_mods:
+                query = ("update tb_evento set "+str(i[0])+" = "+str(i[1])+" where pk_int_id_evento = "+str(i[2])).replace("None","null")
+                cur2.execute(query)
+            conn2.commit()
+
+        #===========================================================================================
+
+        #Obtendo dados atualizados da tabela tb_barraca do banco do 1° e passando para o banco do 2°
+        cur1.execute('select pk_int_id_barraca, var_nome, deletedat, fk_int_id_evento from tb_barraca where updatedat = %s order by pk_int_id_barraca',(ontem,))
+        barracas = cur1.fetchall()
+
+        #Verificando se algum novo registro foi encontrado
+        if len(barracas) > 0:
+            lista_cols = ["var_nome", "deletedat", "fk_int_id_evento"]
+            lista_mods = []
+            querySelect = "select var_nome, deletedat, fk_int_id_evento from tb_barraca where pk_int_id_barraca in ("
+            for i in barracas:
+                querySelect = querySelect+i[0]+","
+            querySelect = (querySelect[:-1])+") order by pk_int_id_barraca"
+            cur2.execute(querySelect)
+            barracas2 = cur2.fetchall()
+
+            for i in barracas2:
+                for c in range(0,len(i)):
+                    if i[c] != barracas[c+1]:
+                        if c == 2:
+                            lista_mods.append([lista_cols[c],barracas[c+1],i[0]])
+                        else:
+                            lista_mods.append([lista_cols[c],"'"+str(barracas[c+1])+"'",i[0]])
+            
+            for i in lista_mods:
+                query = ("update tb_barracas set "+str(i[0])+" = "+str(i[1])+" where pk_int_id_barraca = "+str(i[2])).replace("None","null")
+                cur2.execute(query)
+            conn2.commit()
         
 
     except (Exception, pg.Error) as error:
